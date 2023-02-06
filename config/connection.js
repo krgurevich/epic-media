a04ab31eb6bd8900a22608ffa8b00299d2db981e
krgurevich <kananyan@optonline.net>
2023-02-06 15:01:59 -0500

    modified code in server.js and model/index.js

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 // -- Local Connection to MongoDB  --
-mongoose.connect("mongoose://127.0.0.1:27017/epicDB", {
+mongoose.connect("mongoose://127.0.0.1:27017/epicmediaDB", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
